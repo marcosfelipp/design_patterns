23956726351d09cc1c21a8c220937f8f1d273b80
Marcos Felipe <mfelipe11@yahoo.com.br>
2017-11-30 13:17:08 -0200

    Start implementation of new pattern: composite

---
+++ creational/composite.py
@@ -0,0 +1,46 @@
+'''
+Implementation of File System
+'''
+
+from abc import ABCMeta, abstractmethod
+
+class Client():
+    '''
+    Manipulate objects of composite though Componet interface
+    '''
+
+class Component():
+
+    @abstractmethod
+    def add(self):
+
+    @abstractmethod
+    def remove(self):
+        pass
+
+    @abstractmethod
+    def get_children(self):
+        pass
+
+
+class File():
+    def add(self):
+        pass
+
+    def remove(self):
+        pass
+
+    def get_children(self):
+        pass
+
+
+class Diretory(Component):
+    def __init__(self):
+        pass
+
+    def add(self):
+        pass
+    def remove(self):
+        pass
+    def get_children(self):
+        pass
